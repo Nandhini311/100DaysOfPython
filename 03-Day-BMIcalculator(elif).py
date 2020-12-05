5fcaa69ae12ab8db1c9d1b6e44a7fbc51b80b392
Medusa <63045112+Nandhini311@users.noreply.github.com>
2020-12-05 21:27:24 +0530

    Create 03-Day-BMIcalculator(elif).py

---
+++ 03-Day-BMIcalculator(elif).py
@@ -0,0 +1,12 @@
+bmi = round(weight/height**2)
+
+if(bmi<18.5):
+  print(f"Your bmi is {bmi} ! you are underweight")
+elif(bmi<25):
+  print(f"Your bmi is {bmi} ! you are normal weight")
+elif(bmi<30):
+  print(f"Your bmi is {bmi} ! you are slightly overweight")
+elif(bmi<35):
+  print(f"Your bmi is {bmi} ! you are obese")
+else:
+  print(f"Your bmi is {bmi} ! you are clinically obese")
