b6096692ff1269b431dc61e33e471bc79b3c9941
Medusa <63045112+Nandhini311@users.noreply.github.com>
2020-12-05 22:45:48 +0530

    Update 03-Day-LeapYear.py

--- 03-Day-LeapYear.py
+++ 03-Day-LeapYear.py
@@ -6,4 +6,6 @@ year = int(input("Please Enter the Year Number you wish: "))
 if (( year%400 == 0)or (( year%4 == 0 ) and ( year%100 != 0))):
     print("%d is a Leap Year" %year)
 else:
-    print("%d is Not the Leap Year" %year
+    print("%d is Not the Leap Year" %year)
+    
+#incase of elif any one of the statement will be executed and multiple if any number of the statement can be executed if condition is satisifed
