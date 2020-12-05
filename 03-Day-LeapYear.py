354f24d911bc1f2a73f748014a666bad2a04d972
Medusa <63045112+Nandhini311@users.noreply.github.com>
2020-12-05 22:43:11 +0530

    Create 03-Day-LeapYear.py

---
+++ 03-Day-LeapYear.py
@@ -0,0 +1,9 @@
+# Python Program to Check Leap Year using If Statement
+#it should be divisible by 4 or 400 and not be divisible by 100, for it to be a leap year
+
+year = int(input("Please Enter the Year Number you wish: "))
+
+if (( year%400 == 0)or (( year%4 == 0 ) and ( year%100 != 0))):
+    print("%d is a Leap Year" %year)
+else:
+    print("%d is Not the Leap Year" %year
