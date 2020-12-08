970944651111f897d490cd7013f27f98c8be9eb8
Medusa <63045112+Nandhini311@users.noreply.github.com>
2020-12-08 11:39:09 +0530

    Update RockPaperScissors.py

--- 04-Day-RandomisationAndList/RockPaperScissors.py
+++ 04-Day-RandomisationAndList/RockPaperScissors.py
@@ -32,11 +32,16 @@ print(f"user choice: {user_choice}")
 computer_choice = random.randint(0,3)
 print(f"computer choice: {computer_choice}")
 
-if(user_choice==0 and computer_choice==2):
+if(user_choice>=3 or computer_choice<0):
+  print("You typed an invalid number! You loose honey")
+elif(user_choice==0 and computer_choice==2):
   print("You win!")
+elif(computer_choice==2 and user_choice==0):
+  print("You loose!")
 elif(computer_choice>user_choice):
   print("You loose!")
+elif(user_choice>computer_choice):
+  print("You win!")
 elif(computer_choice == user_choice):
   print("It's a Draw!")
-else:
-  print("You typed an invalid number! You loose honey")
+
