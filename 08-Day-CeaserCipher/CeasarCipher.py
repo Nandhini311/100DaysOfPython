4eb187cd92311d0ec84dfc92a833fa0e235e9f28
Medusa <63045112+Nandhini311@users.noreply.github.com>
2020-12-10 18:26:16 +0530

    Encrypt and decrypt messages

---
+++ 08-Day-CeaserCipher/CeasarCipher.py
@@ -0,0 +1,33 @@
+import art
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+  
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+print(art.logo)
+end = False
+
+while not end:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  shift = shift%26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  again = input("Hey ! Dp you want to restrat the cipher program yes or no").lower()
+  if(again=='no'):
+    end = True
+    print("Boi Boi")
