f04c22884410bc13e9a286c6fea371256b2fb7da
Medusa <63045112+Nandhini311@users.noreply.github.com>
2020-12-30 23:14:04 +0530

    Create turtle.py

---
+++ 16-Day-OOPs_class&object/turtle.py
@@ -0,0 +1,15 @@
+from turtle import Turtle,Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+
+
+#move turtle by 100 step forward
+timmy.forward(100.00)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
