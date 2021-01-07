08939af219c2c565024ca66aef0edd60422f0ea5
Medusa <63045112+Nandhini311@users.noreply.github.com>
2021-01-07 08:30:44 +0530

    Add files via upload

---
+++ 17-Day-Quiz-Game-(class concept)/Quiz-Game/quiz_brain.py
@@ -0,0 +1,27 @@
+class QuizBrain:
+    
+    def __init(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+        self.score = 0
+        
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+      
+    def next_question(self):
+        current_question = self.question_list[self.question_number]  #an object
+        self.question_number += 1 
+        user_answer= input(f"Q. {self.question_number}: {current_question.text} {True/False}")
+        self.check_answer(user_answer,current_question.answer)
+        
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right")
+            
+        else:
+            print("Sorry! Wrong answer")
+            
+        print(f"The correct answer is: {correct_answer}.")
+        print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+        print("\n")
