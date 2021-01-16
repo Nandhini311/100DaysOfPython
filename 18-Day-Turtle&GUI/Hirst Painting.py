9d96bd3ba8ce937779f84e599186a84293393101
Medusa <63045112+Nandhini311@users.noreply.github.com>
2021-01-16 11:29:27 +0530

    Update Hirst Painting.py

--- 18-Day-Turtle&GUI/Hirst Painting.py
+++ 18-Day-Turtle&GUI/Hirst Painting.py
@@ -1,8 +1,8 @@
-import turtle as turtle_module
+import turtle as t
 import random
 
-turtle_module.colormode(255)
-tim = turtle_module.Turtle()
+t.colormode(255)
+tim = t.Turtle()
 tim.speed("fastest")
 tim.penup()
 tim.hideturtle()
@@ -10,7 +10,7 @@ color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), 
 tim.setheading(225)
 tim.forward(300)
 tim.setheading(0)
-number_of_dots = 100
+dots = 100
 
 for dot_count in range(1, number_of_dots + 1):
     tim.dot(20, random.choice(color_list))
@@ -22,14 +22,6 @@ for dot_count in range(1, number_of_dots + 1):
         tim.setheading(180)
         tim.forward(500)
         tim.setheading(0)
-
-
-
-
-
-
-
-
 
 screen = turtle_module.Screen()
 screen.exitonclick()
